b558c0f544ac1176ca31b83c7b089187e3ad4e13
Akas <phelina94@gmail.com>
2024-03-04 16:30:06 +0100

    wip activities

--- atd-api/app/Models/Activity.php
+++ atd-api/app/Models/Activity.php
@@ -9,6 +9,18 @@ class Activity extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',
+        'description',
+        'address',
+        'zipcode',
+        'start_date',
+        'end_date',
+        'donation',
+        'id_type',
+        'archive'
+    ];
+
     public function files(){
         return $this->belongsToMany(File::class, 'activity_files', 'id_activity', 'id_file')->withPivot('archive');
     }

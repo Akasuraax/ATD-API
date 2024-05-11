6f0fb6c49fe63355f124265e66b92f66e02833b2
Akas <phelina94@gmail.com>
2024-05-11 17:02:52 +0200

    fix pdf double trajet

--- atd-api/app/Services/PdfService.php
+++ atd-api/app/Services/PdfService.php
@@ -2,7 +2,9 @@
 
 namespace App\Services;
 
+use App\Http\Controllers\FileController;
 use App\Models\Activity;
+use App\Models\File;
 use TCPDF;
 
 class PdfService
@@ -22,11 +24,19 @@ class PdfService
 
         $pdfFileName = $activity->id . '-' .'journey' . '.pdf';
 
+        $count = File::where('name', 'LIKE', '%'. pathinfo($pdfFileName, PATHINFO_FILENAME) . '%')->where('archive',false)->count();
+        if($count>0) {
+            $file = File::where('name', 'LIKE', '%'. pathinfo($pdfFileName, PATHINFO_FILENAME) . '%')->where('archive',false)->first();
+            app(FileController::class)->deleteActivityFile($activity->id, $file->id);
+        }
+
         $activityFolderPath = public_path('./storage/activities/' . $activity->id);
 
         if (!is_dir($activityFolderPath)) {
             mkdir($activityFolderPath, 0777, true);
         }
+
+
 
         $pdfFilePath = $activityFolderPath . '/' . $pdfFileName;
 
